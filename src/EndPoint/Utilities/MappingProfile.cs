77b86388133987d1910dfed268c6ed1f75e60ebd
alisoltaniorg <alisoltanipv@outlook.com>
2022-09-30 19:26:29 +0330

    Add Mapping Profile

---
+++ src/EndPoint/Utilities/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EndPoint.Entities;
+using EndPoint.ViewModels;
+
+namespace EndPoint.Utilities
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Todo, TodosViewModel>().ReverseMap();
+        }
+    }
+}
