1cdd5b07482d2ff3071bf7e3e27946462892cd65
alisoltaniorg <alisoltanipv@outlook.com>
2022-09-30 13:13:48 +0330

    Add and Configure Enities

---
+++ src/EndPoint/Entities/Common/IEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EndPoint.Entities.Common
+{
+    public interface IEntity
+    {
+    }
+
+    public abstract class AuditableEntity<TKey>
+    {
+        [Key]
+        public TKey Id { get; set; }
+
+        public string CreatedBy { get; set; } = string.Empty;
+
+        public DateTime CreatedOn { get => DateTime.Now; }
+
+        public string? ModifiedBy { get; set; }
+
+        public DateTime? ModifiedOn { get => DateTime.Now; }
+    }
+
+    public class AuditableEntity: AuditableEntity<int>
+    {
+
+    }
+}
---
+++ src/EndPoint/Entities/Todo.cs
@@ -0,0 +1,29 @@
+﻿using EndPoint.Entities.Common;
+
+namespace EndPoint.Entities
+{
+    public class Todo : AuditableEntity, IEntity
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime ReminderDate { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public bool IsStart { get; set; }
+
+        public bool IsResume { get; set; }
+
+        public bool IsStop { get; set; }
+
+        public DateTime AmountTime { get; set; }
+
+        public DateTime RemainingTime { get; set; }
+
+
+    }
+}
