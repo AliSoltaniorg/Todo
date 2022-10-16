4135bf66df643e3093de66e1d8c81415bb10ee96
Ali Soltani <alisoltani.it@gmail.com>
2022-10-16 10:32:32 +0330

    Setup Moq Tests

--- src/EndPoint/Context/AppDbContext.cs
+++ src/EndPoint/Context/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace EndPoint.Context
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
-
+            
         }
 
         public DbSet<Todo> Todos => Set<Todo>();
--- tests/EndPoint.Tests.Unit/Services/TodoServicesTests.cs
+++ tests/EndPoint.Tests.Unit/Services/TodoServicesTests.cs
@@ -2,28 +2,56 @@
 using EndPoint.Entities;
 using EndPoint.Services;
 using EndPoint.Services.Interfaces;
+using EndPoint.Tests.Unit.Helpers;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
 namespace EndPoint.Tests.Unit.Services
 {
     public class TodoServicesTests
     {
-        private readonly Mock<AppDbContext> _appDbContextMock;
-
+        private readonly Mock<IServiceProvider> _serviceProviderMock;
+        private readonly Mock<IAppDbContext> _dbContextMock;
+        private readonly Mock<DbSet<Todo>> _dbSetMock;
+        private readonly ITodoServices _todoServices;
         public TodoServicesTests()
         {
-            _appDbContextMock = new Mock<AppDbContext>();
+            _serviceProviderMock = new Mock<IServiceProvider>();
+
+            _dbContextMock = new Mock<IAppDbContext>();
+
+            _dbSetMock = new Mock<DbSet<Todo>>();
+
+            _dbContextMock.SetupGet(c => c.Todos).Returns(_dbSetMock.Object);
+
+            _serviceProviderMock.Setup(x => x.GetService(typeof(IAppDbContext))).Returns(_dbContextMock.Object);
+
+            _todoServices = new TodoServices(_serviceProviderMock.Object);
         }
 
         [Fact]
         public void Should_Add_Todo()
         {
             //arrange
-            //var services = new TodoServices(_appDbContextMock.Object);
 
             //act
+            Func<Task<Todo>> add = async()=> await _todoServices.Add(new Todo());
+            add();
 
+            //assert
+            _dbSetMock.Verify(c => c.AddAsync(It.IsAny<Todo>(),It.IsAny<CancellationToken>()));
+            _dbContextMock.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public void Should_Edit_Todo()
+        {
+            //arrange
+
+
+            //act
 
             //assert
         }
